6b59ca653aa6c3d323f4738e9ea86acc3e5c23ae
mandioquynha <machado.amandag@gmail.com>
2023-08-24 00:41:35 -0300

    build: criação da aplicação backend e instalação de dependências iniciais

---
+++ study_chess/src/test/java/studychess/StudyChessApplicationTests.java
@@ -0,0 +1,13 @@
+package studychess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
