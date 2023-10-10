fbcd7035d49efa626037a3a3cb9cb28233838a4a
mandioquynha <machado.amandag@gmail.com>
2023-10-09 23:55:13 -0300

    corrigida constraint dos dados

--- study_chess/src/main/resources/db/migrations/V4__create-table-checkmate.sql
+++ study_chess/src/main/resources/db/migrations/V4__create-table-checkmate.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `checkmate` (
   `checkmate_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
-  `type` VARCHAR(100) UNIQUE NOT NULL,
+  `type` VARCHAR(100) NOT NULL,
   `text` VARCHAR NOT NULL,
   `notation` VARCHAR,
   PRIMARY KEY (`id`)
